9ba267709a7f7ed1aff3925ae58a22549cf08465
Bernardo Alves <bernardoalves330@gmail.com>
2024-08-06 02:21:54 -0300

    installed zod and added error handler to app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import { router } from './routes/index'
+import { errorHandlerMiddleware } from './middleware/ErrorHandlerMiddleware'
 
 const app = express()
 
 app.use(express.json())
 app.use(router)
+app.use(errorHandlerMiddleware)
 
 export { app }
